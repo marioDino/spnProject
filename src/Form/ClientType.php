817311f33d9f268548219db3bb7aeb3bfcb743de
Aina <ainarakotomalala227@gmail.com>
2019-11-08 13:58:43 +0100

    ajout des autres composants

---
+++ src/Form/ClientType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Client;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class ClientType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nom',TextType::class,[
+                'attr'=>[
+                    'class'=>'form-control'
+                ]
+            ])
+            ->add('adresse',TextType::class,[
+                'attr'=>[
+                    'class'=>'form-control'
+                ]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Client::class,
+        ]);
+    }
+}
